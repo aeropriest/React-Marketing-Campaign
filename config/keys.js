608261bcd69066adf394ee043673afddeb17d665
aeropriest <ashok.jaiswal@gmail.com>
2021-08-09 14:47:57 +0800

    Added production env variabel

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,5 @@
 //key.js - no need to commit this
-if( process.env.NODE_ENV == 'production' ){
+if( process.env.NODE_ENV === 'production' ){
     module.exports = require('./prod');
 }else{
     module.exports = require('./dev');
