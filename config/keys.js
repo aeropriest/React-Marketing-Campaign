254c376e0d6d3715ad8c0a4ad72a2ff1d23ef878
aeropriest <ashok.jaiswal@gmail.com>
2021-08-09 14:41:47 +0800

    Use the production env

--- config/keys.js
+++ config/keys.js
@@ -1,12 +1,11 @@
 //key.js - no need to commit this
-/*
 if( process.env.NODE_ENV == 'production' ){
     module.exports = require('./prod');
 }else{
     module.exports = require('./dev');
 }
-*/
 
+/*
 //dev.js - no need to commit this
 module.exports = {
     googleClientID : '572820114901-7h075qqe8i5d9mamogcad60oid5gna41.apps.googleusercontent.com',
@@ -14,3 +13,4 @@ module.exports = {
     mongoURI: 'mongodb+srv://dbUser:123dbUser123@reactmarketingcampaign.ga76a.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
     cookieKey: 'sdfsfsadawd'
 }
+*/
