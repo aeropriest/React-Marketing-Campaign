9b5244edd4c2ea03aafe08981307c7d74716a860
aeropriest <ashok.jaiswal@gmail.com>
2021-08-09 23:52:30 +0800

    Added cation handler

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,22 +1,32 @@
-import React from 'react';
+import React, { Component } from 'react';
 import {BrowserRouter, Route} from 'react-router-dom';
+import { connect } from 'react-redux';
+import * as actions from '../actions';
 
-
-const Header = () => <h1>Header</h1>
+import Header from './Header';
 const Dashboard = () => <h1>Dashboard</h1>
 const SurveyNew = () => <h1>SurveyNew</h1>
-const Landing = () => <h1>Landing!</h1>
+const Landing = () => <h1>Landing</h1>
 
-const App = () => {
-    return(
-        <div>
-            <BrowserRouter>
-                <div>
-                    <Route path='/' component={Landing} />
-                </div>
-            </BrowserRouter>
-        </div>
-    );
+class App extends Component {
+    componentDidMount(){
+        this.props.fetchUser();
+    }
+
+    render(){
+        return (
+            <div className="container">
+                <BrowserRouter>
+                    <div>
+                        <Header />
+                        <Route exact path='/' component={Landing} />
+                        <Route path='/surveys' component={Dashboard} />
+                        <Route path='/surveys/new' component={SurveyNew} />
+                    </div>
+                </BrowserRouter>
+            </div>
+        );
+    }
 };
 
-export default App;
+export default connect(null, actions)(App);
