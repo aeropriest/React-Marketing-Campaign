3b0ad45bf1bd3c83b1c73d675fa3fc2142d8ea02
aeropriest <ashok.jaiswal@gmail.com>
2021-08-11 12:49:38 +0800

    Add server side build steps

--- config/keys.js
+++ config/keys.js
@@ -1,19 +1,27 @@
+/*
 //key.js - no need to commit this
 if( process.env.NODE_ENV === 'production' ){
     module.exports = require('./prod');
 }else{
     module.exports = require('./dev');
 }
+*/
 
-/*
 //dev.js - no need to commit this
 module.exports = {
-    googleClientID : '572820114901-7h075qqe8i5d9mamogcad60oid5gna41.apps.googleusercontent.com',
-    googleClientSecret : 'g45ePTQAlxan-rEXrW-DZlD9',
+    googleClientID : '572820114901-b0qnmdag3ncvle0irkqgp2aca63c92qi.apps.googleusercontent.com',
+    googleClientSecret : 'z_mFDUgIZ8ZbrIXi2Yq2jfLT',
     mongoURI: 'mongodb+srv://dbUser:123dbUser123@reactmarketingcampaign.ga76a.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',
-    cookieKey: 'sdfsfsadawd'
+    cookieKey: 'uiweikjdsdhbuybweloiqowlkajnnjhsaduek'
 }
 
-STRIPE PUBLIC : pk_test_51JMvYrCYAFDOk6O8BjHidVKs6bTa6xErOOI0V9Ou9MZNlz1nH6dC0wG1oWWTWx3HQPujs9BU6OmPJ6JcWnUiVI7y00AwjoqSsz
-SECRET: sk_test_51JMvYrCYAFDOk6O8ZSFG7ycSxc0y1JoypHWSQ7MUhYJgyC5SdJpnzBrCXe6yQLmapl4PEnYRwkYFv2x6s7YPM8JE00DNndrBF1
+//STRIPE PUBLIC : pk_test_51JMvYrCYAFDOk6O8BjHidVKs6bTa6xErOOI0V9Ou9MZNlz1nH6dC0wG1oWWTWx3HQPujs9BU6OmPJ6JcWnUiVI7y00AwjoqSsz
+//SECRET: sk_test_51JMvYrCYAFDOk6O8ZSFG7ycSxc0y1JoypHWSQ7MUhYJgyC5SdJpnzBrCXe6yQLmapl4PEnYRwkYFv2x6s7YPM8JE00DNndrBF1
+
+
+/*
+1020488102764-0v3ghtkndl96156rqq64hq87q8le1j6r.apps.googleusercontent.com
+emaily
+572820114901-b0qnmdag3ncvle0irkqgp2aca63c92qi.apps.googleusercontent.com
+z_mFDUgIZ8ZbrIXi2Yq2jfLT
 */
--- routes/billingRoutes.js
+++ routes/billingRoutes.js
@@ -1,9 +1,11 @@
 const { stripePublishableKey } = require('../config/keys');
 const keys = require('../config/keys');
 const stripe = require('stripe')(keys.stripeSecretKey);
+const requireLogin = require('../middleware/requireLogin');
+
 
 module.exports = app => {
-    app.post('/api/stripe', async (req,res) => {
+    app.post('/api/stripe', requireLogin, async (req,res) => {
         const charge = await stripe.charges.create({
             amount: 500,
             currency: 'usd',
@@ -11,6 +13,9 @@ module.exports = app => {
             source: req.body.id
         });
 
+        req.user.credits += 5;
+        const user = await req.user.save();
         console.log(charge);
+        res.send(user);
     });
 };
